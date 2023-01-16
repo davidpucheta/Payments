2a0cb14b107c6bf075f131220333b476acc3ba6f
David Reyes Pucheta <davidpucheta@gmail.com>
2023-01-16 12:48:04 -0600

    Add data layer
    Add DB file, model and context

---
+++ Payments/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Payments.Model.Data;
+
+namespace Payments.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<Card> Card { get; set; }
+    
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Card>().ToTable("Card");
+    }
+}
---
+++ Payments/Model/Data/Card.cs
@@ -0,0 +1,10 @@
+﻿namespace Payments.Model.Data;
+
+public class Card
+{
+    public int Id { get; set; }
+
+    public string CardNumber { get; set; }
+
+    public decimal Balance { get; set; }
+}
